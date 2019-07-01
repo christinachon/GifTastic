cee1820dceb21f7055c98086e98f4fbcbe6748e8
Christina <christinachon90@gmail.com>
2019-06-30 18:54:19 -0700

    generate gifs with button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,15 +5,38 @@ $(document).ready(function(){
     renderButtons();
 })
 
-function alertFoodName() {
-    alert($(this).attr("data-name"));
+function displayGifs() {
+    $("#gifs-go-here").empty();
+    var food = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      food + "&api_key=qN5lX7Krpj8pqRVhhFxT83JsvWXo2R5B&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div>");
+          var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
+          var foodImage = $("<img>");
+          foodImage.attr("src", results[i].images.fixed_height.url);
+          gifDiv.prepend(p);
+          gifDiv.prepend(foodImage);
+          $("#gifs-go-here").prepend(gifDiv);
+          console.log(response)
+        }
+      });
+
 }
 
 function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < foods.length; i++) {
         var a = $("<button>");
-        a.addClass("food btn btn-info btn-sm m-1");
+        a.addClass("food btn btn-sm m-1");
         a.attr("data-name", foods[i]);
         a.text(foods[i]);
         $("#buttons-view").append(a);
@@ -28,5 +51,5 @@ $("#add-food").on("click", function (event) {
     $("#food-input").val(" ");
 });
 
-$(document).on("click", ".food", alertFoodName);
+$(document).on("click", ".food", displayGifs);
 renderButtons();
